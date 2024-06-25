7f1e95a7ed1072e88b10339495d9f8a8d6e0d710
fynitecorp <tahir.warraich@fynite.ai>
2024-06-25 12:02:19 -0500

    Add models/apply_condition.sql

--- models/apply_condition.sql
+++ models/apply_condition.sql
@@ -3,14 +3,13 @@
 
     WITH source_data AS (
         SELECT *
-        FROM {{ source('sample20_fivetran_metadata', 'jampack_sample_data') }}
+        FROM { source('sample20_fivetran_metadata', 'jampack_sample_data') }
     )
 
     SELECT
         *,
         CASE
-            WHEN {{ var('ticket_condition') }} THEN 1
+            WHEN { var('condition') } THEN 1
             ELSE 0
-        END AS ticket_condition_flag
+        END AS test_column
     FROM source_data
-    
